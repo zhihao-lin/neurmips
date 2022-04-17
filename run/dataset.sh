485ba72957358c710604b9aead01d5f0ba1c778b
Zhi-Hao Lin <r08942062@ntu.edu.tw>
2022-04-17 18:33:09 +0800

    [220417] download dataset: Tanks&Temples

--- run/dataset.sh
+++ run/dataset.sh
@@ -3,3 +3,8 @@ wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download
 unzip data/replica.zip -d data/
 rm data/replica.zip
 
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1nt1F-GxDwkQXOIyO-VZwy230fO7iqLam' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1nt1F-GxDwkQXOIyO-VZwy230fO7iqLam" -O data/TanksAndTemple.zip && rm -rf /tmp/cookies.txt
+unzip data/TanksAndTemple.zip -d data/
+mv data/data/TanksAndTemple data/
+rm -r data/data
+rm data/TanksAndTemple.zip
